72226f5981f36aa835c4973b40e301a27a48b1f2
unknown <abhinavbajimaya1@gmail.com>
2022-11-16 16:43:57 +0545

    change test

--- src/controllers/unitconverter.py
+++ src/controllers/unitconverter.py
@@ -4,4 +4,5 @@ from odoo.http import request
 class UnitConverter(http.Controller):
     @http.route('/unit-converter', type='http', auth='public', website=True)
     def converter(self, **kw):
+        
         return request.render('src.unitcon', {})
