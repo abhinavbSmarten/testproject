edbf4cea72ae4704e72fea92ed7126fd10b744c1
unknown <abhinavbajimaya1@gmail.com>
2022-11-16 12:33:29 +0545

    Folder Structure and Unit Converter 1.Initialized folder structure 2.Units Converter views and logic developed

---
+++ src/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'cpnepal',
+    'version': '1.0.0',
+    'category': 'realestate',
+    'sequence': 1,
+    'summary': 'cp nepal operations',
+    'description': """Module to manage cp nepal operations.""",
+    'author': 'Smarten Technologies',
+    'website': '',
+    'depends': ['base','website'],
+    'data': [
+        "security/ir.model.access.csv",
+        "views/unitcon.xml",
+    ],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+    'assets': {},
+    'license': 'AGPL-3'
+}
