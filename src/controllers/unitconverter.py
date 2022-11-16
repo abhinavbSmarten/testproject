5d8b330594e7b1c43ec2684b0f7fb04b47ce0185
unknown <abhinavbajimaya1@gmail.com>
2022-11-16 12:33:29 +0545

    Folder Structure and Unit Converter 1.Initialized folder structure 2.Units Converter views and logic developed

---
+++ src/controllers/unitconverter.py
@@ -0,0 +1,7 @@
+from odoo import http
+from odoo.http import request
+
+class UnitConverter(http.Controller):
+    @http.route('/unit-converter', type='http', auth='public', website=True)
+    def converter(self, **kw):
+        return request.render('src.unitcon', {})
